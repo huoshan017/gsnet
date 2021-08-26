e4e3ba0a179cfc2140414b796471b4c6644794c3
huoshan017 <huoshan017@hotmail.com>
2021-08-26 13:44:42 +0800

    update

--- session.go
+++ session.go
@@ -3,6 +3,7 @@ package gsnet
 type Session struct {
 	conn IConn
 	id   uint64
+	data interface{}
 }
 
 func NewSession(conn IConn, id uint64) *Session {
@@ -22,4 +23,12 @@ func (s *Session) Close() {
 
 func (s *Session) GetId() uint64 {
 	return s.id
+}
+
+func (s *Session) SetData(d interface{}) {
+	s.data = d
+}
+
+func (s *Session) GetData() interface{} {
+	return s.data
 }
